0c91ec2841799b8a066145300971933da648ba55
spectacular-system-6224 <d.leslie707@gmail.com>
2020-01-23 09:04:25 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
-
+  p "num1 + num2 = #{num1 + num2}"
 end
 
 def subtraction(num1, num2)
-
+  p "num1 - num2 = #{num1 - num2}"
 end
 
 def division(num1, num2)
-
+  p "num1 / num2 = #{num1 / num2}"
 end
 
 def multiplication(num1, num2)
-
+  p "num1 * num2 = #{num1 * num2}"
 end
 
 def modulo(num1, num2)
-
+  p "num1 % num2 = #{num1 % num2}"
 end
 
 def square_root(num)
-
+  p "num ^ (1/2) = #{Math.sqrt(num1)}"
 end
+
+num1 = 8
+num2 = 4
+num = 16
+
+addition(num1, num2)
+subtraction(num1, num2)
+division(num1, num2)
+multiplication(num1, num2)
+modulo(num1, num2)
+square_root(num)
